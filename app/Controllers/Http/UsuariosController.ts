99e1ec8e1a9970446a2de7bbebfc01453bea03a9
Yeison Cuervo Basurto <ycuervob@unal.edu.co>
2023-02-02 23:27:21 -0500

    Pretty formatted

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -2,45 +2,45 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Usuario from 'App/Models/Usuario'
 
 export default class UsuariosController {
-    public async getListarUsuarios() : Promise<Usuario[]>{
+    public async getListarUsuarios(): Promise<Usuario[]> {
         const user = await Usuario.all();
         return user;
     }
 
-    public async getListarUsuariosYPerfil() : Promise<Usuario[]>{
+    public async getListarUsuariosYPerfil(): Promise<Usuario[]> {
         const user = await Usuario.query().preload('perfil');
         return user;
     }
 
-    public async getListarUsuariosyPublicaciones() : Promise<Usuario[]>{
+    public async getListarUsuariosyPublicaciones(): Promise<Usuario[]> {
         const user = await Usuario.query().preload('publicaciones');
         return user;
     }
 
-    public async getListarUsuariosGrupos() : Promise<Usuario[]>{
+    public async getListarUsuariosGrupos(): Promise<Usuario[]> {
         const user = await Usuario.query().preload('usuario_grupos');
         return user;
     }
 
-    public async setRegistrarUsuario({request,response}: HttpContextContract){
-        const dataUsuario = request.only(['nombre_usuario','codigo_usuario','telefono_usuario','email_usuario','contrasena']);
-        try{
+    public async setRegistrarUsuario({ request, response }: HttpContextContract) {
+        const dataUsuario = request.only(['nombre_usuario', 'codigo_usuario', 'telefono_usuario', 'email_usuario', 'contrasena']);
+        try {
             const codigoUsuario = dataUsuario.codigo_usuario;
-            const usuarioExistente : Number = await this.getValidarUsuarioExistente(codigoUsuario);
-            if(usuarioExistente === 0){
+            const usuarioExistente: Number = await this.getValidarUsuarioExistente(codigoUsuario);
+            if (usuarioExistente === 0) {
                 await Usuario.create(dataUsuario);
-                response.status(200).json({message:'Usuario registrado correctamente'});
-            }else{
-                response.status(400).json({message:'Error, el codigo usuario ya se encuentra registrado'});
+                response.status(200).json({ message: 'Usuario registrado correctamente' });
+            } else {
+                response.status(400).json({ message: 'Error, el codigo usuario ya se encuentra registrado' });
             }
-        }catch(error){
+        } catch (error) {
             console.log(error);
-            response.status(500).json({message:'Error al registrar el usuario'});
+            response.status(500).json({ message: 'Error al registrar el usuario' });
         }
     }
 
-    public async getValidarUsuarioExistente(codigoUsuario : Number) : Promise<Number>{
-        const total = await Usuario.query().where({'codigo_usuario':codigoUsuario}).count('*').from('usuarios');
+    public async getValidarUsuarioExistente(codigoUsuario: Number): Promise<Number> {
+        const total = await Usuario.query().where({ 'codigo_usuario': codigoUsuario }).count('*').from('usuarios');
         return parseInt(total[0]['count(*)']);
     }
 }
